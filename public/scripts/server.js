56cd94dd6a3b9725dba503bdb9908a05eef878ce
Omair Muhi <omairmuhi@icloud.com>
2020-12-13 09:47:53 -0500

    Subject: Add /public folder Problem:NA Solution:NA Note:Move scripts and styles here

--- public/scripts/server.js
+++ public/scripts/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const fs = require('fs');
 
 // DB director
-const dbDirectory = path.resolve(__dirname, "../db");
+const dbDirectory = path.resolve(__dirname, "../../db");
 
 // Sets up the Express App
 const app = express();
@@ -15,13 +15,20 @@ let PORT = process.env.PORT || 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // Routes: 3xGET, 1xPOST, 1xDELETE
 // GET: rest.sendFile(index.html)
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.get('/', (req, res) => {
+    console.log("Serving index.html");
+    res.sendFile(path.join(__dirname, '../../index.html'))
+});
 
 // GET: res.sendFile(notes.html)
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../notes.html')));
+app.get('/notes', (req, res) => {
+    console.log("Serving notes.html");
+    res.sendFile(path.join(__dirname, '../../notes.html'))
+});
 
 // GET: Return all saved notes as JSON from db.json
 app.get('/api/notes', (req, res) => {
